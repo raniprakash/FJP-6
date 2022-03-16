ae96ac33b1667015aa66b98f5c2d4602cefa3122
raniprakash <rani.bhuti30@gmail.com>
2022-03-16 20:13:30 +0530

    strings

---
+++ module2/strings.js
@@ -0,0 +1,40 @@
+let str="I am a string";
+console.log(str);
+
+
+//length of a string
+
+let len=str.length;
+console.log(len);
+
+//slice method
+
+let slicedArr=str.slice(2,5);
+console.log(slicedArr);
+
+//replace method
+
+let replacedStr=str.replace("am","was");
+console.log(replacedStr);
+
+//to Uppercase
+let upperStr=str.toUpperCase();
+console.log(upperStr);
+
+
+//to Lowercase
+let lowerStr=str.toLowerCase();
+console.log(lowerStr);
+
+//concatenation
+
+let firstName="Rani";
+let lastName="Prakash";
+let fullName=firstName.concat(lastName);
+console.log(fullName);
+
+//split function
+
+let splitted=str.split(" ");
+console.log(splitted);
+
