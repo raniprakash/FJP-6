f21e0f277d9f6c70a9528c5a5c09f520dd1caffc
raniprakash <rani.bhuti30@gmail.com>
2022-03-16 22:02:29 +0530

    objects.js created

---
+++ module2/object.js
@@ -0,0 +1,13 @@
+//Empty Object
+let obj={};
+console.log(obj);
+
+
+let obj1={
+    "Name":"Rani Prakash",
+    "Age":22,
+    "PhoneNo": 8563254698,
+    lastName :"Prakash"    //keys are by default considered as strings. Whether you mention them in quotes or not, it doesn't matter.
+}
+
+console.log(obj1);
