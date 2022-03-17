614bd66c6e85e15abf4211d11cc9f733a6952694
raniprakash <rani.bhuti30@gmail.com>
2022-03-18 00:56:47 +0530

    custom modules in node

---
+++ module2/node/module/test.js
@@ -0,0 +1,8 @@
+let calc=require("./calculator");
+
+console.log(calc);
+
+console.log(calc.addition(10,5));
+console.log(calc.substraction(10,5));
+console.log(calc.multiplication(10,5));
+console.log(calc.division(10,5));
