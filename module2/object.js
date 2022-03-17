a60828b046a5dfc107606437a508d00a0411c304
raniprakash <rani.bhuti30@gmail.com>
2022-03-17 11:44:50 +0530

    object and accessing keys of objects

--- module2/object.js
+++ module2/object.js
@@ -11,3 +11,42 @@ let obj1={
 }
 
 console.log(obj1);
+
+let capAmerica={
+    Name: "Steve Rogers",
+    Age:120,
+    friends:["Natasha Romanoff", 'Bruce Banner','Tony Stark','Bucky'],
+    Address: {
+        City: 'Queens',
+        State: 'New York'
+    },
+    saysHi:function(){
+        console.log("Captain America says Hi!!");
+    }
+
+}
+
+//whole object
+//console.log(capAmerica);
+
+
+//Name of capAmerica
+console.log(capAmerica.Name);
+
+//Age
+console.log(capAmerica.Age);
+
+//Friends
+console.log(capAmerica.friends);
+console.log(capAmerica.friends[0]);
+
+
+//Address
+console.log(capAmerica.Address);
+
+//city
+console.log(capAmerica.Address.City);
+
+//sayHi
+console.log(capAmerica.saysHi());
+capAmerica.saysHi();
