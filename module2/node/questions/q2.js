3154892bf4397d99026ea39872e4906db9de4bc5
raniprakash <rani.bhuti30@gmail.com>
2022-03-20 00:27:12 +0530

    q2,q3 solved

--- module2/node/questions/q2.js
+++ module2/node/questions/q2.js
@@ -1,2 +1,29 @@
 //Q. given an array arr=[audio,video,image,software,documents,applications,other]
 //   make folder for each element in the given array and inside each folder make 4 files of that type
+
+let fs=require("fs");
+let path=require("path");
+
+let arr = ['Audio','Video','Software','Documents','Applications','Others'];
+let nameArr = ['abc','def','ghi','jkl'];
+let extArr = ['.mp3','.mp4','.exe','.pdf','.apk','.rar'];
+
+let organisePath=path.join(__dirname,"Organised");
+
+if(!(fs.existsSync("Organised"))){
+    fs.mkdirSync("Organised");
+
+    for(let i=0;i<arr.length;i++)
+    {
+        let folderpath=path.join(organisePath,arr[i])
+        if(!(fs.existsSync(folderpath)))
+            fs.mkdirSync(folderpath);
+        for(let j=0;j<nameArr.length;j++)
+        {
+            let fileName=nameArr[j]+extArr[i];
+            let filepath=path.join(folderpath,fileName);
+            fs.writeFileSync(filepath,"");
+        }
+    }
+
+}
--- module2/node/questions/q3.js
+++ module2/node/questions/q3.js
@@ -1,1 +1,12 @@
 //Q. copy index.html file from module1 to a new folder inside module2 having name html.
+
+let fs=require("fs");
+let path=require("path");
+
+
+let sourcePath=path.join(__dirname,"..","..","..","\\module1\\project1\\index.html");
+console.log(sourcePath);
+let destinationPath=path.join(__dirname,"..","..","index.html");
+console.log(destinationPath);
+
+fs.copyFileSync(sourcePath,destinationPath);
