b15e677eca6f9a371be2826d3f8679585a9508e0
raniprakash <rani.bhuti30@gmail.com>
2022-03-22 15:57:31 +0530

    Extensions differentiated into folders

--- module2/project/fileOrganiser.js
+++ module2/project/fileOrganiser.js
@@ -8,6 +8,7 @@
 
 
 let fs = require("fs");
+let path=require("path");
 //Task 1: to take input
 let folderpath = process.argv[2];
 //console.log(folderpath);
@@ -21,10 +22,39 @@ let folderpath = process.argv[2];
 
 let folderExists = fs.existsSync(folderpath);
 
+let extensions = {
+    Audio:[".mp3"],
+    Video:[".mp4",".mkv"],
+    Document:[".doc",".xlsx",".pdf",".txt"],
+    Image:[".jpeg",".jpg",".png",".gif"],
+    Software:[".exe"]
+};
+
 if(folderExists){
     let files=fs.readdirSync(folderpath);
-    console.log(files);
+    // console.log(files);
+    for(let i=0;i<files.length;i++)
+    {
+        let ext=path.extname(files[i]);
+        let nameOfFolder=giveNameOfFolder(ext);
+        console.log("Ext-->",ext,"FolderName-->",nameOfFolder);
+    }
 }
 else{
     console.log("Please enter valid pathname!!!");
 }
+
+//Task 3: Differentiating files
+function giveNameOfFolder(ext)
+{
+    for(key in extensions){
+        let extArr=extensions[key];
+        for(let i=0;i<extArr.length;i++)
+        {
+            if(extArr[i]==ext)
+                return key;
+        }
+    }
+    return 'Others';
+}
+   
