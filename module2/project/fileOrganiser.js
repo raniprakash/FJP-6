0da68c6f8a1015ddda9c4c8dd2649d0ab6a64d56
raniprakash <rani.bhuti30@gmail.com>
2022-03-22 15:05:28 +0530

    Download-folder to be organised creates

--- module2/project/fileOrganiser.js
+++ module2/project/fileOrganiser.js
@@ -1,8 +1,30 @@
 
-let input = process.argv;
-console.log(input);
-//['pathToNode','pathToFile','Input passed in the terminal']
+// let input = process.argv;
+// console.log(input);
+// //['pathToNode','pathToFile','Input passed in the terminal']
 
-let inputArr = process.argv;
-console.log(inputArr[2]);
+// let inputArr = process.argv;
+// console.log(inputArr[2]);
 
+
+let fs = require("fs");
+//Task 1: to take input
+let folderpath = process.argv[2];
+//console.log(folderpath);
+
+
+/*
+    We assume that the folder we are organising doesn't contain any sub-folders, it only contains files.
+*/
+
+//Task 2: to read the folder
+
+let folderExists = fs.existsSync(folderpath);
+
+if(folderExists){
+    let files=fs.readdirSync(folderpath);
+    console.log(files);
+}
+else{
+    console.log("Please enter valid pathname!!!");
+}
